ad290c33839c3e1fd6450a96762ff0c47b740866
MinnKhantKyaww <minkhant199701@gmail.com>
2019-12-24 14:55:19 +0500

    Update

--- app/src/main/java/com/example/attendancekt/MainActivity.kt
+++ app/src/main/java/com/example/attendancekt/MainActivity.kt
@@ -2,20 +2,37 @@ package com.example.attendancekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.attendancekt.ui.member.MemberListFragment
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_member_edit.*
 
 class MainActivity : AppCompatActivity() {
-    //https://dribbble.com/shots/8556810-Attendance-App-UI-1-Dribbble-Invite
-    //https://dribbble.com/shots/8567947-Attendance-Dashboard-Report-Mobile-App-Design/attachments/838311?mode=media
-    //https://search.muz.li/inspiration/mobile-app-design-inspiration/
-    //https://www.uplabs.com/posts/office-hour-attendance-app
-    //https://dribbble.com/shots/5266591-inClass-Attendance-App
-    //https://search.muz.li/YzFiOTBlYmNj
-    //https://dribbble.com/shots/3764318-Attendance-App-For-Students
-    //https://dribbble.com/shots/5359961-Mapchat-iOS-App-design/attachments
-    //https://www.behance.net/gallery/69427253/LUOKKAPP-Student-Attendance-Mobile-App-(Case-Study)
-    //https://csform.com/product/matta-material-design-android-ui-template-theme-app/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        NavigationUI.setupWithNavController(toolbar, Navigation.findNavController(this, R.id.nav_host_fragment))
+        //addMember.setOnClickListener { v -> Navigation.findNavController(v).navigate(R.id.action_memberListFragment_to_memberEditFragment)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
---
+++ app/src/main/java/com/example/attendancekt/ui/member/MemberEditFragment.kt
@@ -0,0 +1,36 @@
+package com.example.attendancekt.ui.member
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MemberEditFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
